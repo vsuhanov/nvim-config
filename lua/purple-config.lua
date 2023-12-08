0b1f4f656374145eb577c9441d0a417018ce8d0a
vitaly <vitalijs.suhanovs@gmail.com>
2023-12-09 00:14:59 +0200

    harpoon remapping

--- lua/purple-config.lua
+++ lua/purple-config.lua
@@ -80,8 +80,8 @@ local harpoon = require("harpoon")
 harpoon:setup()
 -- REQUIRED
 
-vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
-vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<leader>aa", function() harpoon:list():append() end)
+vim.keymap.set("n", "<C-i>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
