f6dab19c3a2cf8bcdcce35f69cc19f90ec207451
vitaly <vitalijs.suhanovs@gmail.com>
2023-12-09 00:17:11 +0200

    is this committed?

--- lua/purple-config.lua
+++ lua/purple-config.lua
@@ -87,3 +87,4 @@ vim.keymap.set("n", "<C-i>", function() harpoon.ui:toggle_quick_menu(harpoon:lis
 -- vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
 -- vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
 -- vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+--
