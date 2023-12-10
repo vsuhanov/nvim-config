3c1fe2509fc388493332bebaaf63bcad69f8b820
vitaly <vitalijs.suhanovs@gmail.com>
2023-12-10 14:22:25 +0200

    added bindings for harpoon list items

--- lua/purple-config.lua
+++ lua/purple-config.lua
@@ -83,10 +83,10 @@ harpoon:setup()
 vim.keymap.set("n", "<leader>aa", function() harpoon:list():append() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
--- vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
--- vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
--- vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
--- vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
 
 require("gp").setup({
  	openai_api_key = os.getenv("OPENAI_API_KEY"),
