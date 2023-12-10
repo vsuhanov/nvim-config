b93ef2acac414ff817512d2afcccd3ac455916d8
vitaly <vitalijs.suhanovs@gmail.com>
2023-12-10 20:35:43 +0200

    add autocmd for arduino comment string support and auto reloading for init.vim

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -6,3 +6,19 @@ vim.api.nvim_create_autocmd(
     command = "silent! update",
   }
 )
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "arduino",
+    callback = function()
+        vim.opt_local.commentstring = "// %s"
+    end,
+})
+
+-- Define an augroup and autocmd to auto-source the init.vim file on write
+local source_init_group = vim.api.nvim_create_augroup("source_init", { clear = true })
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+    group = source_init_group,
+    pattern = "init.vim",
+    command = "silent! source %",
+})
