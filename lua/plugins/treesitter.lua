736ce02f1265e29668cbbb7b8d0b603baab8ca9e
vitaly <vitalijs.suhanovs@gmail.com>
2023-12-11 20:55:19 +0200

    add more treesitter parsers and reformat the list

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,20 @@
 require 'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "arduino", "markdown", "markdown_inline" },
+  ensure_installed = { 
+    "c",
+    "lua",
+    "vim",
+    "vimdoc",
+    "query",
+    "arduino",
+    "markdown",
+    "markdown_inline",
+    "java",
+    "groovy",
+    "html",
+    "css",
+    "javascript",
+    "typescript"
+  },
   sync_install = false,
   auto_install = true,
   highlight = {
