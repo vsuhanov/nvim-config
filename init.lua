94a304f6e0f4b47a0f380475b51f5eef6f7cb27e
vitaly <vitalijs.suhanovs@gmail.com>
2024-02-01 21:10:40 +0200

    load local-config if the file does exist

--- init.lua
+++ init.lua
@@ -5,3 +5,9 @@ require('commands')
 require('set')
 require('purple-config')
 require('windows-stuff')
+
+-- ignore errors: local-config may not exist
+pcall(function()
+  require("local-config")
+end)
+
