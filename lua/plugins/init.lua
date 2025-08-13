280f3ce1641aa17b288427eac5c3b0d5979433b7
Vitaliy Suhanov <vitaliy.suhanov@ctco.lv>
2025-08-13 10:03:39 +0300

    fix plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -61,6 +61,16 @@ require("lazy").setup({
     "smjonas/inc-rename.nvim",
     opts = {}
   },
+  {
+    "rest-nvim/rest.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      opts = function(_, opts)
+        opts.ensure_installed = opts.ensure_installed or {}
+        table.insert(opts.ensure_installed, "http")
+      end,
+    }
+  }
   -- "neovim/nvim-lspconfig",
   -- 'mfussenegger/nvim-jdtls',
   -- 'mustache/vim-mustache-handlebars',
