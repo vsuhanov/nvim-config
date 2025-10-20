2dd6e8d71510fb31f0349ba694383921af59c879
vitaly <vitalijs.suhanovs@gmail.com>
2025-10-20 22:11:10 +0300

    add region highlight plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -63,6 +63,14 @@ require("lazy").setup({
     "vsuhanov/toggle-file.nvim",
     config = function() require('plugins.toggle-file') end
   },
+  {
+    'vsuhanov/region-highlight.nvim',
+    config = function()
+      require('region-highlight').setup({
+        -- your configuration here
+      })
+    end
+  },
   { "nvim-tree/nvim-tree.lua", config = function() require('plugins.nvim-tree') end },
   "nvim-tree/nvim-web-devicons",
   { "yorickpeterse/nvim-window",    config = function() require('plugins.nvim-window') end },
@@ -170,5 +178,3 @@ require("lazy").setup({
     end,
   },
 })
-
-
